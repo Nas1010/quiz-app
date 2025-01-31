341431ae456908b3f81bb518ec2e15e9c02bb310
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-01-31 23:27:07 +0000

    added css to answers, added a data.js file

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -6,17 +6,25 @@ const Quiz = () => {
     let [index, setIndex] = useState(0);
     let [question, setQuestion] = useState(data[index])
 
+    const checkAns = (e, ans) => {
+        if (question.ans === ans) {
+            e.target.classList.add("correct");
+        }
+        else {
+            e.target.classList.add("wrong");
+        }
+}
+
   return (
       <div className='container'>
           <h1>Macbeth Quiz</h1>
           <hr />
           <h2> {index + 1} {question.question} </h2>
           <ul>
-              <li>{question.option1}</li>
-              <li>{question.option2}</li>
-              <li>{question.option3}</li>
-              <li>{question.option4}</li>
-              
+              <li onClick={(e)=> {checkAns(e, 1)}}>{question.option1}</li>
+              <li onClick={(e)=> {checkAns(e, 2)}}>{question.option2}</li>
+              <li onClick={(e)=> {checkAns(e, 3)}}>{question.option3}</li>
+              <li onClick={(e)=> {checkAns(e, 4)}}>{question.option4}</li>
           </ul>
           <button>Next</button>
           <div className='index'>1 of 5 questions</div>
