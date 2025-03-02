6e5ce5e5124135b6bcf0b4b0cf2f4de5aec7b749
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-03-02 10:47:57 +0000

    This commit will increase data-set from 5 to 25

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,43 +1,204 @@
 export const data = [
-    {
-      question: "Who says the line, 'Fair is foul, and foul is fair'?",
-      option1: "Banquo",
-      option2: "Macbeth",
-      option3: "The Witches",
-      option4: "Lady Macbeth",
-      ans: 3
-    },
-    {
-      question: "Who kills Duncan?",
-      option1: "Macbeth",
-      option2: "Macduff",
-      option3: "Banquo",
-      option4: "Malcolm",
-      ans: 1
-    },
-    {
-      question: "Who says, 'Out, out, brief candle!'?",
-      option1: "Macbeth",
-      option2: "Lady Macbeth",
-      option3: "Banquo",
-      option4: "Macduff",
-      ans: 1
-    },
-    {
-      question: "Who is crowned King of Scotland after Duncan?",
-      option1: "Malcolm",
-      option2: "Banquo",
-      option3: "Macbeth",
-      option4: "Donalbain",
-      ans: 3
-    },
-    {
-      question: "Who is the first to see Banquo's ghost at the banquet?",
-      option1: "Lady Macbeth",
-      option2: "Macbeth",
-      option3: "Ross",
-      option4: "Lennox",
-      ans: 2
-    }
-  ];
+  {
+    question: "Who says the line, 'Fair is foul, and foul is fair'?",
+    option1: "Banquo",
+    option2: "Macbeth",
+    option3: "The Witches",
+    option4: "Lady Macbeth",
+    ans: 3
+  },
+  {
+    question: "Who kills Duncan?",
+    option1: "Macbeth",
+    option2: "Macduff",
+    option3: "Banquo",
+    option4: "Malcolm",
+    ans: 1
+  },
+  {
+    question: "Who says, 'Out, out, brief candle!'?",
+    option1: "Macbeth",
+    option2: "Lady Macbeth",
+    option3: "Banquo",
+    option4: "Macduff",
+    ans: 1
+  },
+  {
+    question: "Who is crowned King of Scotland after Duncan?",
+    option1: "Malcolm",
+    option2: "Banquo",
+    option3: "Macbeth",
+    option4: "Donalbain",
+    ans: 3
+  },
+  {
+    question: "Who is the first to see Banquo's ghost at the banquet?",
+    option1: "Lady Macbeth",
+    option2: "Macbeth",
+    option3: "Ross",
+    option4: "Lennox",
+    ans: 2
+  },
+  {
+    question: "Who warns Lady Macduff that danger is approaching?",
+    option1: "A Messenger",
+    option2: "Ross",
+    option3: "Banquo",
+    option4: "Lennox",
+    ans: 1
+  },
+  {
+    question: "Who discovers Duncan’s dead body?",
+    option1: "Macduff",
+    option2: "Lennox",
+    option3: "Malcolm",
+    option4: "Donalbain",
+    ans: 1
+  },
+  {
+    question: "What title is Macbeth given after his victory in battle?",
+    option1: "King of Scotland",
+    option2: "Thane of Glamis",
+    option3: "Thane of Cawdor",
+    option4: "Prince of Cumberland",
+    ans: 3
+  },
+  {
+    question: "How does Lady Macbeth die?",
+    option1: "She is murdered",
+    option2: "She falls from a tower",
+    option3: "She is poisoned",
+    option4: "She takes her own life",
+    ans: 4
+  },
+  {
+    question: "What vision does Macbeth see before killing Duncan?",
+    option1: "A floating dagger",
+    option2: "Banquo's ghost",
+    option3: "A bloody child",
+    option4: "A crowned child with a tree in its hand",
+    ans: 1
+  },
+  {
+    question: "Who kills Macbeth?",
+    option1: "Banquo",
+    option2: "Macduff",
+    option3: "Malcolm",
+    option4: "Siward",
+    ans: 2
+  },
+  {
+    question: "What prophecy do the witches NOT give Macbeth?",
+    option1: "That he will be King",
+    option2: "That Banquo's sons will be kings",
+    option3: "That he will be Thane of Cawdor",
+    option4: "That he will be defeated by an army led by Malcolm",
+    ans: 4
+  },
+  {
+    question: "What does Lady Macbeth say about Macbeth's nature?",
+    option1: "'Yet I do fear thy nature; It is too full o’ the milk of human kindness.'",
+    option2: "'Look like the innocent flower, but be the serpent under't.'",
+    option3: "'All the perfumes of Arabia will not sweeten this little hand.'",
+    option4: "'What’s done cannot be undone.'",
+    ans: 1
+  },
+  {
+    question: "What does the bloody child apparition tell Macbeth?",
+    option1: "Beware Macduff",
+    option2: "No man born of woman shall harm Macbeth",
+    option3: "Macbeth will be king forever",
+    option4: "Banquo’s sons will be kings",
+    ans: 2
+  },
+  {
+    question: "Why does Malcolm pretend he is worse than Macbeth?",
+    option1: "To test Macduff’s loyalty",
+    option2: "Because he is actually worse",
+    option3: "Because he wants Macduff to leave",
+    option4: "To convince Macduff to return to Scotland",
+    ans: 1
+  },
+  {
+    question: "What does Macbeth see at the banquet that no one else does?",
+    option1: "A floating dagger",
+    option2: "The ghost of Duncan",
+    option3: "The ghost of Banquo",
+    option4: "The witches",
+    ans: 3
+  },
+  {
+    question: "What does Macbeth say after killing Duncan?",
+    option1: "'I am afraid to think what I have done.'",
+    option2: "'Is this a dagger which I see before me?'",
+    option3: "'To be thus is nothing, but to be safely thus.'",
+    option4: "'Tomorrow, and tomorrow, and tomorrow.'",
+    ans: 1
+  },
+  {
+    question: "What does Lady Macbeth famously try to wash off her hands?",
+    option1: "Blood",
+    option2: "Dirt",
+    option3: "Perfume",
+    option4: "Ink",
+    ans: 1
+  },
+  {
+    question: "Who is the last person to speak in the play?",
+    option1: "Macduff",
+    option2: "Malcolm",
+    option3: "Ross",
+    option4: "Lennox",
+    ans: 2
+  },
+  {
+    question: "What does Macbeth think will make him invincible?",
+    option1: "His armor",
+    option2: "The witches' prophecies",
+    option3: "His soldiers",
+    option4: "His love for Lady Macbeth",
+    ans: 2
+  },
+  {
+    question: "What does Lady Macbeth say when she sleepwalks?",
+    option1: "'Fair is foul, and foul is fair.'",
+    option2: "'Look like the innocent flower, but be the serpent under't.'",
+    option3: "'Out, damned spot! Out, I say!'",
+    option4: "'Is this a dagger which I see before me?'",
+    ans: 3
+  },
+  {
+    question: "How do the witches greet Macbeth?",
+    option1: "'Thane of Glamis! Thane of Cawdor! King hereafter!'",
+    option2: "'Hail, King of Scotland!'",
+    option3: "'Beware Macduff! Beware the Thane of Fife!'",
+    option4: "'Double, double, toil and trouble!'",
+    ans: 1
+  },
+  {
+    question: "Why does Macbeth kill Banquo?",
+    option1: "Because Banquo is plotting against him",
+    option2: "Because Banquo’s descendants are prophesied to be kings",
+    option3: "Because the witches tell him to",
+    option4: "Because Banquo killed Duncan",
+    ans: 2
+  },
+  {
+    question: "Where does Malcolm flee after Duncan's murder?",
+    option1: "England",
+    option2: "Ireland",
+    option3: "France",
+    option4: "Norway",
+    ans: 1
+  },
+  {
+    question: "Who is the Thane of Fife?",
+    option1: "Lennox",
+    option2: "Ross",
+    option3: "Macduff",
+    option4: "Banquo",
+    ans: 3
+  }
+];
+
   
